6c0ba34d48dd8815145874957425fb0aae28b1b0
lindseylabrie <lindseylabrie7@gmail.com>
2022-04-12 10:27:09 -0500

    Update StreamPredRealDeal.R

--- StreamPredRealDeal.R
+++ StreamPredRealDeal.R
@@ -6,9 +6,10 @@ library(ggplot2)
 library(janitor)
 
 # Loading the data
-
+# looking ONLY at hellgrammites and oplonaeschna
 data <- read_csv("past_time_sample_data.csv") %>% 
-  select(-X9,-X10) 
+  select(-X9,-X10) %>% 
+  filter(data$`Timed Sample`==c("1", "Timed 1"))
 
 CaveCreek <- data %>% 
   filter(data$Stream=="Cave Creek")
@@ -24,12 +25,38 @@ EastTurkey <-data %>%
 CaveCreek$Site <- factor(CaveCreek$Site,      # Reordering group factor levels
                       levels = c("Research Station","John Hands","Herb Martyr", "Site 9", "Site 10"))
 
-CaveCreek %>% ggplot(aes(x=Species,y='Specimens/m^2'))+
-  facet_grid(cols = vars(Date))+
-  geom_col(aes(color=Species, fill=Species))+
+# Visualizing the data over the years 
+# trying to see the differences in SITES, YEARS, and DENSITIES the same time
+
+# Looking at data by year
+
+CC2022 <- CaveCreek %>% filter(CaveCreek$Date==2022)
+CC2013 <- CaveCreek %>% filter(CaveCreek$Date==2013)
+CC2015 <- CaveCreek %>% filter(CaveCreek$Date==2015)
+CC2016 <- CaveCreek %>% filter(CaveCreek$Date==2016)
+
+# Seeing if there's a difference between Timed 1 and Timed 2
+CC2022 %>% ggplot(aes(x=Site, y=`Specimens/m^2`))+
+  geom_point(aes(color=Species))+
+  facet_grid(cols = vars(`Timed Sample`))
+# kind of looks like there is a difference between timed one and two,
+# but would need to make a model to see if that is true.
+
+CC2013 %>% ggplot(aes(x=Site, y=`Specimens/m^2`))+
+  geom_point(aes(color=Species))
+
+CC2015 %>% ggplot(aes(x=Site, y=`Specimens/m^2`))+
+  geom_point(aes(color=Species))
+
+CC2016 %>% ggplot(aes(x=Site, y=`Specimens/m^2`))+
+  geom_point(aes(color=Species))
+
+CaveCreek %>% ggplot(aes(x=Site,y=`Specimens/m^2`))+
+  # facet_grid(cols = vars(Date))+
+  geom_col(aes(color=Site, fill=Species))+
   # geom_text(aes(label='Specimens/m^2'), position = position_stack(vjust = 0.5),size=3.5)+
   # theme_linedraw()+
   labs(x="",
-       y="Individual Specimens")+
-  theme(axis.ticks.x = element_blank(),
-        axis.text.x = element_blank())
+       y="Density of Specimens per Square Meter")+
+  theme_light()
+
