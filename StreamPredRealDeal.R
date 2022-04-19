a7602fac003d47cdf9383830c4c989c6ac761820
lindseylabrie <lindseylabrie7@gmail.com>
2022-04-19 08:51:03 -0500

    edit code

--- StreamPredRealDeal.R
+++ StreamPredRealDeal.R
@@ -4,13 +4,14 @@ library(tidybayes)
 library(dplyr)
 library(ggplot2)
 library(janitor)
+library(ggpubr)
+library(brms)
 
 # Loading the data
 # looking ONLY at hellgrammites, rrs (red rock skimmer), and oplonaeschna (riffle darner)
 
 # getting the data for the years that are not 2022
 data <- read_csv("past_time_sample_data.csv") %>% 
-  # select(-x9,-x10) %>% 
   clean_names() %>% 
   mutate(timed_sample = case_when(timed_sample=="Timed 1"~"1",
                    timed_sample=="Timed 2"~"2",
@@ -18,10 +19,13 @@ data <- read_csv("past_time_sample_data.csv") %>%
          altitude = case_when(site=="John Hands"~"1715",
                               site=="Herb Martyr"~"1760",
                               site=="Research Station"~"1615",
-                              site=="Site 9"~"1825")) %>% 
+                              site=="Site 9"~"1825",
+                              site=="Site 10"~"1900")) %>% 
   filter(species %in% c("hellgrammite", "oplonaeschna", "RRS"),
-         date %in% c("2013","2015","2016"))
-
+         date %in% c("2013","2015","2016")) %>% 
+  select(-x9, -x10)
+           
+           
 # 2022 combined timed sample data
 combined_timed <- read_csv("combined_time_sample_data.csv") %>% 
   clean_names() %>% 
@@ -147,3 +151,114 @@ main_preds <- CC2022 %>% ggplot(aes(x=site,y=specimens_m_2))+
         legend.position = "none")
 
 ggsave(main_preds, file= "plots/main_preds.png", dpi = 350, width = 6.25, height = 8, units = "in")
+
+
+
+#### some sort of analysis , start from "getting priors" section####
+
+# density over altitude changes
+
+altitude <- specimens_m_2 ~ altitude
+class(altitude)
+
+boxplot(specimens_m_2 ~ altitude*species, 
+        data = CC2022,
+        color = "species",
+        varwidth = TRUE)
+
+summary(aov(specimens_m_2 ~ altitude),
+        data = CC2022)
+
+# getting priors
+get_prior(specimens_m_2 ~ species + altitude + species*altitude, 
+          data = all_timed,
+          family = gaussian())
+
+# making my model
+altitude <- brm(specimens_m_2 ~ species + altitude + species*altitude, 
+                           data = all_timed,
+                           family = gaussian(),
+                           cores = 1, chains = 1, iter = 1000,
+                           sample_prior = "yes",
+                           file="models/altitude.rds",
+                           file_refit = "on_change")
+
+# conditional effects, taking all individuals into account
+plot(conditional_effects(altitude, re_formula = NULL), points = T)
+# conditional effects, showing the mean difference
+plot(conditional_effects(altitude), points = T)
+
+pp_check(altitude)
+pp_check(altitude, type = "hist")
+
+saveRDS(altitude, "models/altitude.rds")
+
+# # conditional effects, manual plotting
+# as_draws_df(length_bsr_negbinom)
+# 
+# cond_effect_length <- conditional_effects(length_bsr_negbinom)
+# cond_effect_length$length_s
+# 
+# cond_effect_length$lenth_s %>% 
+#   ggplot(aes(x=length_s)) +
+#   geom_pointrange(aes(y=estimate__, ymin=lower__, ymax=upper__))+
+#   geom_point(data = length_bsr_negbinom$data, aes(x=length_s, y=combined_egg_total))+
+#   theme_default()
+# # it keeps saying "estimate__ not found"
+# 
+# cond_data_length <- length_bsr_negbinom$data %>% distinct(length_s, combined_egg_total)
+# 
+# posts_length <- add_epred_draws(length_bsr_negbinom, newdata= length_bsr_negbinom$data %>% 
+#                                   distinct(length_s) , re_formula = NA)
+# 
+# 
+# posts_length_all <- add_predicted_draws(length_bsr_negbinom, newdata= length_bsr_negbinom$data %>% 
+#                                           distinct(length_s) , re_formula = NA)
+# 
+# d_length <- d %>% distinct(length_mm, length_s)
+# 
+# PosteriorLength <- posts_length_all %>%
+#   group_by(length_s) %>% 
+#   left_join(d_length) %>% 
+#   median_qi(.prediction) %>% 
+#   mutate(length_mm = (length_s*sd(d$length_mm)) + mean(d$length_mm)) %>% 
+#   ggplot(aes(x = length_mm, y = .prediction)) +
+#   geom_line() +
+#   geom_ribbon(aes(ymin = .lower, ymax = .upper),
+#               alpha = 0.2) +
+#   geom_point(data = d, 
+#              aes(y = combined_egg_total)) +
+#   labs(title= "Blue Sucker Fecundity Prediction",
+#        subtitle="Large grey bar incorporates the variation in individuals",
+#        x="Length (mm)",
+#        y="Predicted total egg count")
+# 
+# ggsave(PosteriorLength, file = "plots/PosteriorLength.png", dpi = 750, width = 7, height = 5,
+#        units = "in")
+# # This model incorporates all individuals, and not JUST the mean.
+# 
+# PosteriorLengthMean <- posts_length %>%
+#   group_by(length_s) %>% 
+#   left_join(d_length) %>% 
+#   median_qi(.epred) %>% 
+#   mutate(length_mm = (length_s*sd(d$length_mm)) + mean(d$length_mm)) %>% 
+#   ggplot(aes(x = length_mm, y = .epred)) +
+#   geom_line() +
+#   geom_ribbon(aes(ymin = .lower, ymax = .upper),
+#               alpha = 0.2) +
+#   geom_point(data = d, 
+#              aes(y = combined_egg_total)) +
+#   labs(title= "Blue Sucker Mean Fecundity Prediction",
+#        subtitle="Grey bar incorporates only the variation in the mean egg count",
+#        x="Length (mm)",
+#        y="Predicted total egg count")
+# 
+# ggsave(PosteriorLengthMean, file = "plots/PosteriorLengthMean.png", dpi = 750, width = 7, height = 5,
+#        units = "in")
+# 
+# 
+# 
+# 
+# 
+# 
+# 
